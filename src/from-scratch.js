f279ce7ea5f025754ed0c17729e427772de898b2
Mei Zhu <maggiez0613@gmail.com>
2024-07-11 15:06:47 -0400

    q1 to q3 done

--- src/from-scratch.js
+++ src/from-scratch.js
@@ -1,13 +1,31 @@
 class Quadrilateral {
-
+  constructor(side1, side2, side3, side4) {
+    this.side1 = side1; 
+    this.side2 = side2; 
+    this.side3 = side3;
+    this.side4 = side4;
+  }
+  getPerimeter() {
+    return this.side1 + this.side2 + this.side3 + this.side4; 
+  }
 }
 
-class Rectangle {
-
+class Rectangle extends Quadrilateral {
+  constructor(side1, side2) {
+    super(side1, side2, side1, side2);
+  }
+  getArea() {
+    return this.side1 * this.side2;
+  }
 }
 
-class Square {
-
+class Square extends Rectangle {
+  constructor(side1) {
+    super(side1, side1, side1, side1)
+  }
+  getDiagonal() {
+    return Math.sqrt((this.getArea() * 2));
+  }
 }
 
 /* Be creative with this one! */
